16a44b75d6cc7ddfbdd1dfd37ed7c1ccc91c2462
Vu Duc Tuyen <tuyen@naxam.net>
2017-05-30 00:06:34 +0700

    Remove internal static interfaces - LockableScrollChangedListener - CardBrandChangeListener - CardNumberCompleteListener - ExpiryDateEditListener

---
+++ qs/StripeQs/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Com.Stripe.Android.View;
+
+namespace StripeQs
+{
+    [Activity(Label = "StripeQs", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var mCardInputWidget = FindViewById<CardInputWidget>(Resource.Id.card_input_widget);
+
+        }
+    }
+}
+
--- qs/StripeQs/Properties/AssemblyInfo.cs
+++ qs/StripeQs/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using Android.App;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Naxam.Stripe.Droid")]
-[assembly: AssemblyDescription("Xamarin Binding Library for Stripe Android SDK")]
+[assembly: AssemblyTitle("StripeQs")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NAXAM COMPANY LIMITED")]
-[assembly: AssemblyProduct("X Bindings")]
+[assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("Copyright (c) 2017 NAXAM")]
 [assembly: AssemblyTrademark("NAXAM")]
 [assembly: AssemblyCulture("")]
@@ -18,7 +18,7 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("4.0.2.1")]
+[assembly: AssemblyVersion("1.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
