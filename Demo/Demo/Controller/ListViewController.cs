9f0f17d8505c003cd77716155d908ee3aed56a18
Do Thanh Binh <binh@naxam.net>
2017-05-31 21:32:38 +0700

    edit ListviewController in demo

--- Demo/Demo/Controller/ListViewController.cs
+++ Demo/Demo/Controller/ListViewController.cs
@@ -10,24 +10,29 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Com.Stripe.Android.Model;
+using Java.Lang;
 
 namespace Demo.Controller
 {
+
     public class ListViewController
     {
 
-        private SimpleAdapter mAdatper;
-        private IList<IDictionary<string, object>> mCardTokens = new List<IDictionary<string, object>>();
-        private Context mContext;
 
+        private SimpleAdapter mAdatper;
+        private List<IDictionary<string, object>> mCardTokens;
+        private Context mContext;
+        ListView MyListView;
         public ListViewController(ListView listView)
         {
-            mContext = listView.Context;
+            MyListView = listView;
+            mContext = MyListView.Context;
+            mCardTokens = new List<IDictionary<string, object>>(); 
             mAdatper = new SimpleAdapter(mContext, mCardTokens,
                     Resource.Layout.list_item_layout,
                     new string[] { "last4", "tokenId" },
                     new int[] { Resource.Id.last4, Resource.Id.tokenId });
-            listView.Adapter = mAdatper;
+            MyListView.Adapter = mAdatper;
         }
 
         public void AddToList(Token token)
@@ -38,10 +43,10 @@ namespace Demo.Controller
         public void AddToList(string last4, string tokenId)
         {
             string endingIn = mContext.GetString(Resource.String.endingIn);
-            Dictionary<string, string> map = new Dictionary<string, string>();
+            Dictionary<string, object> map = new Dictionary<string, object>();
             map.Add("last4", endingIn + " " + last4);
             map.Add("tokenId", tokenId);
-            mCardTokens.Add(map as IDictionary<string, object>);
+            mCardTokens.Add(map);
             mAdatper.NotifyDataSetChanged();
         }
     }
