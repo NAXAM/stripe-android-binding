cd1b0400920c0226940c75c5069afcba502c1942
Do Binh <binh@naxam.net>
2017-09-17 14:49:18 +0700

    update to version 5.0.0 (non example)

---
+++ Demo/Demo/Service/StripeService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Retrofit2.Http;
+using Okhttp3;
+using IO.Reactivex.Internal.Operators;
+
+namespace Demo.Service
+{
+    public interface StripeService
+    {
+        [FormUrlEncoded]
+        [POST(Value = "ephemeral_keys")]
+        IObservable<ResponseBody> CreateEphemeralKey([FieldMap]IDictionary<string, string> apiVersionMap);
+    }
+}
